cf609fcfcec2c6dc56c5844cfc88ea785a76fc0e
jyoti-kushwah <jyotikushwaha0406@gmail.com>
2022-12-21 20:21:45 +0530

    erfg

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
+// setInterval(time,1000);
 
+ function  time(){
+    const times = new Date();
+    let  hour = times.getHours();
+    const  min = times.getMinutes();
+    let  sec = times.getSeconds();
+    let ampm = hour >= 12 ? "PM" : "AM";
+    // document.getElementById("time").innerHTML=hour + ":" + min + ":"+ sec + ":" 
+      //  setInterval(time,1000);
+      if (hour === 0){
+        hour = 12;
+      }
+      if(hour >12){
+        hour = hour - 12;
+      }
+        document.getElementById("hh").innerText = hour;
+        document.getElementById("mm").innerText = min;
+        document.getElementById("ss").innerText = sec;
+        document.getElementById("ampm").innerHTML = ampm;
 
-function  time(){
-    let times = new Date ();
-    let hour = times.getHours();
-    let min = times.getMinutes();
-    let sec = times.getSeconds();
-    document.getElementById("time").innerHTML=hour + ":" + min + ":"+ sec + ":" ;
-    //  setInterval(time,1000);
+        setTimeout(time,1000);
+ }
+ time();
+ 
+// if (hour === 0){
+//   hour = 12;
+// }
+// if(hour >12){
+//   hour = hour - 12;
 
-}
-time();
-setInterval(function(){
-    time();
-
-},1000);
+//   document.getElementById("hh").innerText = hour;
+//   document.getElementById("mm").innerText = min;
+//   document.getElementById("ss").innerText = sec;
+//   document.getElementById("ampm").innerHTML = ampm;
+// document.getElementById("hh").innerText = hour;
+// document.getElementById("mm").innerText = min;
+// document.getElementById("ss").innerText = sec;
+// document.getElementById("ampm").innerHTML = ampm;
